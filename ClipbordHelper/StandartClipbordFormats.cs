12080a505ee8cdcade207a29c736f63c86703cc1
Stadub <dima_@live.at>
2014-02-21 22:11:29 +0300

    [+] Added standard clipbord formats [~] fixerd spell error

---
+++ ClipbordHelper/StandartClipbordFormats.cs
@@ -0,0 +1,10 @@
+﻿namespace ClipbordHelper
+{
+    public enum StandartClipbordFormats
+    {
+        Bitmap = 2, Dib = 8, Dibv5 = 17, Dif = 5, DspBitmap = 0x0082, DspEnhMetafile = 0x008E, DspMetafilePict = 0x0083, DspText = 0x0081,
+        EnhMetafile = 14, GdioOjFirst = 0x0300, GdiObjLast = 0x03FF, HDrop = 15, Locale = 16, MetafilePict = 3, Oemtext = 7,
+        OwnerDisplay = 0x0080, Palette = 9, PenData = 10, PrivateFirst = 0x0200, PrivateLast = 0x02FF, Riff = 11, SyLk = 4,
+        Text = 1, Tiff = 6, UnicodeText = 13, Wave = 12
+    }
+}
