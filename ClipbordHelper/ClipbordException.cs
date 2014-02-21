46b117f0fdd164da41937940b5d410cdbf0e9d13
Stadub <dima_@live.at>
2014-02-21 21:03:07 +0300

    Initial commit

---
+++ ClipbordHelper/ClipbordException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ClipbordHelper
+{
+    [Serializable]
+    public class ClipbordException : Exception
+    {
+        public ClipbordException() { }
+
+        public ClipbordException(string message) : base(message) { }
+
+        public ClipbordException(string message, Exception inner) : base(message, inner) { }
+
+        protected ClipbordException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
