4fe03b280b09b110c4ec46bf3938e6b2533f2ca4
Stadub <dima_@live.at>
2014-10-31 09:05:13 +0300

    ~cleaned from legacy code

--- ClipboardHelper/FormatProviders/HtmlFormatProvider.cs
+++ ClipboardHelper/FormatProviders/HtmlFormatProvider.cs
@@ -78,32 +78,6 @@ namespace ClipboardHelper.FormatProviders
             );
 
 
-       
-        //// Replace the matched text in the InputText using the replacement pattern
-        // string result = regex.Replace(InputText,regexReplace);
-
-        //// Split the InputText wherever the regex matches
-        // 
-
-        //// Capture the first Match, if any, in the InputText
-        // Match m = regex.Match(InputText);
-
-        //// Capture all Matches in the InputText
-        // MatchCollection ms = regex.Matches(InputText);
-
-        //// Test to see if there is a match in the InputText
-        // bool IsMatch = regex.IsMatch(InputText);
-
-        //// Get the names of all the named and numbered capture groups
-        // string[] GroupNames = regex.GetGroupNames();
-
-        //// Get the numbers of all the named and numbered capture groups
-        // int[] GroupNumbers = regex.GetGroupNumbers();
-
-
-
-
-
         public override string FormatId
         {
             get { return "HTML Format"; }
