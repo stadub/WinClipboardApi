5a2ddcb4302f21f1b5d5c8ff5da6a643bf8613a1
Stadub <dima_@live.at>
2015-02-01 01:07:26 +0300

    Added simplest type mapper

---
+++ Utils.Test/TypeMapperTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Utils.Test
+{
+    [TestClass]
+    public class TypeMapperTest
+    {
+        class A
+        {
+             public bool a { get; set; }
+        }
+        class B
+        {
+            public bool a { get; set; }
+        }
+        [TestMethod]
+        public void ShouldMapType()
+        {
+            var mapper = new TypeMapper();
+            var b=mapper.MapTo<B>(new A {a = true});
+            Assert.IsNotNull(b);
+            Assert.IsTrue(b.a==true);
+        }
+    }
+}
---
+++ Utils/TypeMapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Utils
+{
+    public class TypeMapper
+    {
+        public TResult MapTo<TResult>(object source)
+        {
+            var sourceType = source.GetType();
+            var sourceProps=GetPublicNotIndexedProperties(sourceType);
+
+            var destType = typeof(TResult);
+            var destProps = GetPublicNotIndexedProperties(destType).ToLookup(info => info.Name);
+
+            if(sourceType.IsGenericType || destType.IsGenericType)
+                throw new TypeNotSupportedException(sourceType, "Generic types are not supported");
+
+            var ctor = destType.GetConstructors();
+            if(ctor.Length != 1 )
+                throw new TypeNotSupportedException(sourceType, "Only default constructor is supported");
+
+            var destObject=Activator.CreateInstance<TResult>();
+
+            foreach (var propertyInfo in sourceProps)
+            {
+                if(destProps.Contains(propertyInfo.Name))
+                {
+                    var destProp = destProps[propertyInfo.Name].Single();
+
+                    var sourceValue = propertyInfo.GetValue(source);
+                    if (propertyInfo.PropertyType != destProp.PropertyType)
+                    {
+                        var convertedValue = Convert.ChangeType(sourceValue, destProp.PropertyType);
+                        destProp.SetValue(destObject, convertedValue);
+                    }
+                    else
+                        destProp.SetValue(destObject, sourceValue);
+                }
+            }
+            return destObject;
+        }
+
+        private IEnumerable<PropertyInfo> GetPublicNotIndexedProperties(Type type)
+        {
+            //work only with public Not special and not Index properties
+            IEnumerable<PropertyInfo> props = type
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(x => !x.IsSpecialName && x.GetIndexParameters().Length == 0);
+            return props;
+        }
+    }
+}
